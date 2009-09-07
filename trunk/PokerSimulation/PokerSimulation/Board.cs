92ae3ba674385435bef1097dbdcc906dd6083b99
Ahota231 <Ahota231@67adeefe-9b59-11de-bf47-b33d4a03a5a8>
2009-09-07 23:54:13 +0000

    Beginning code

---
+++ trunk/PokerSimulation/PokerSimulation/Board.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace PokerSimulation
+{
+    class Board
+    {
+        #region Variables
+        private List<Image> boardCards;
+        #endregion
+
+        #region Properties
+        public List<Image> BoardCards
+        {
+            get { return boardCards; }
+            set { boardCards = value; }
+        }
+        #endregion
+
+        #region Methods
+        #endregion
+
+        
+    }
+}
---
+++ trunk/PokerSimulation/PokerSimulation/StartForm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Resources;
+
+namespace PokerSimulation
+{
+    public partial class StartForm : Form 
+    {
+        public StartForm()
+        {
+            InitializeComponent();
+            setupCards();
+        }
+
+        #region Variables
+        private List<Card> fullDeck;
+        private List<Card> boardCards;
+        private string debugText;
+        #endregion
+
+        #region Events
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        #endregion
+        
+        #region Methods
+        public void setupCards()
+        {
+            boardCards = new List<Card>(3);
+            boardCards.Add(new Card());
+            boardCards.Add(new Card("Ace_Club"));
+            boardCards.Add(new Card("Five_Spade"));
+            
+        }
+        public void drawOnBoard()
+        {
+            
+            pictureBox1.Image = (Image)boardCards[0].Image;
+            pictureBox2.Image = (Image)boardCards[1].Image;
+            pictureBox3.Image = (Image)boardCards[2].Image;
+            debugText = boardCards.ToString();
+            answerEntryText.Text = debugText;
+            
+                
+        }
+        #endregion
+
+        private void beginButton_Click(object sender, EventArgs e)
+        {
+            drawOnBoard();
+        }
+
+    }
+}
