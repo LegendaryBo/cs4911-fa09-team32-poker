9f8a698e58144ca6c7c22762446c4614e78b6b0c
john.w.crawford <john.w.crawford@67adeefe-9b59-11de-bf47-b33d4a03a5a8>
2009-10-26 04:34:20 +0000

    Split Card, Hand and HandGenerator out of Deck.  Added helpers to Card and finished commenting.

---
+++ CS 4911 Poker Simulator/PokerSimLib4911/PokerSimLib4911/Card.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PokerSimLib4911
+{
+    /// <summary>
+    /// A card has a certain suit and rank, or is a wild card.
+    /// </summary>
+    public class Card
+    {
+        #region private fields
+
+        private static Dictionary<String, Rank> _rankMappings = new Dictionary<string, Rank>();
+        private static Dictionary<String, Suit> _suitMappings = new Dictionary<string, Suit>();
+
+        #endregion
+
+        #region public properties
+
+        public Suit Suit { get; internal set; }
+        public Rank Rank { get; internal set; }
+        public bool IsWild
+        {
+            get
+            {
+                return (Suit == Suit.UNKNOWN || Rank == Rank.UNKNOWN);
+            }
+        }
+
+        #endregion
+
+        #region constructors
+
+        static Card()
+        {
+            #region create dictionary for mapping rank strings to rank values
+
+            // Add the short representations of each rank to the dictionary
+            _rankMappings.Add("A", Rank.ACE);
+            _rankMappings.Add("2", Rank.TWO);
+            _rankMappings.Add("3", Rank.THREE);
+            _rankMappings.Add("4", Rank.FOUR);
+            _rankMappings.Add("5", Rank.FIVE);
+            _rankMappings.Add("6", Rank.SIX);
+            _rankMappings.Add("7", Rank.SEVEN);
+            _rankMappings.Add("8", Rank.EIGHT);
+            _rankMappings.Add("9", Rank.NINE);
+            _rankMappings.Add("T", Rank.TEN);
+            _rankMappings.Add("J", Rank.JACK);
+            _rankMappings.Add("Q", Rank.QUEEN);
+            _rankMappings.Add("K", Rank.KING);
+            _rankMappings.Add("U", Rank.UNKNOWN);
+
+            // Add each rank's full name to the dictionary
+            String[] rankStrings = Enum.GetNames(typeof(Rank));
+            foreach (string r in rankStrings)
+            {
+                _rankMappings.Add(r, (Rank)Enum.Parse(typeof(Rank), r));
+            }
+
+            #endregion
+
+            #region create dictionary for mapping suit strings to suit values
+
+            // Add the short representations of each suit to the dictionary
+            _suitMappings.Add("C", Suit.CLUBS);
+            _suitMappings.Add("D", Suit.DIAMONDS);
+            _suitMappings.Add("H", Suit.HEARTS);
+            _suitMappings.Add("S", Suit.SPADES);
+            _suitMappings.Add("U", Suit.UNKNOWN);
+
+            // Add each rank's full name to the dictionary
+            String[] suitStrings = Enum.GetNames(typeof(Suit));
+            foreach (string s in suitStrings)
+            {
+                _suitMappings.Add(s, (Suit)Enum.Parse(typeof(Suit), s));
+            }
+
+            #endregion
+        }
+
+        /// <summary>
+        /// Constructor for a Card.
+        /// </summary>
+        /// <param name="suit">The Suit of the card.</param>
+        /// <param name="rank">The Rank of the card.</param>
+        public Card(Suit suit, Rank rank)
+        {
+            Suit = suit;
+            Rank = rank;
+        }
+
+        /// <summary>
+        /// Constructor for a Card.
+        /// </summary>
+        /// <param name="rank">A single character representing the card's rank.</param>
+        /// <param name="suit">A single character representing the card's suit.</param>
+        public Card(char rank, char suit) : this(rank.ToString(), suit.ToString()) { }
+
+        /// <summary>
+        /// Constructor for a Card.
+        /// </summary>
+        /// <param name="rank">The card's rank as a string.</param>
+        /// <param name="suit">The card's suit as a string.</param>
+        public Card(string rank, string suit)
+        {
+            rank = rank.ToUpper();
+            suit = suit.ToUpper();
+
+            if (!_rankMappings.ContainsKey(rank))
+            {
+                throw new ArgumentException("The rank string {" + rank + "} specified for the Card constructor is invalid.");
+            }
+            else
+            {
+                Rank = _rankMappings[rank];
+            }
+
+            if (!_suitMappings.ContainsKey(suit))
+            {
+                throw new ArgumentException("The suit string {" + suit + "} specified for the Card constructor is invalid.");
+            }
+            else
+            {
+                Suit = _suitMappings[suit];
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Retrieves the valid string representations for a Card's rank.
+        /// </summary>
+        /// <returns>An array of valid strings for a Card's Rank.</returns>
+        public string[] ValidRankStrings()
+        {
+            return _rankMappings.Keys.ToArray();
+        }
+
+        /// <summary>
+        /// Retrieves the valid string representations for a Card's suit.
+        /// </summary>
+        /// <returns>An array of valid strings for a Card's Suit.</returns>
+        public string[] ValidSuitStrings()
+        {
+            return _suitMappings.Keys.ToArray();
+        }
+
+        /// <summary>
+        /// Checks if a string is a valid Rank.
+        /// </summary>
+        /// <param name="rank">The string to check.</param>
+        /// <returns>True if the string is a valid Rank, otherwise false.</returns>
+        public static bool IsValidRank(string rank)
+        {
+            rank = rank.ToUpper();
+
+            if (_rankMappings.ContainsKey(rank))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Determines if a string is a valid Suit.
+        /// </summary>
+        /// <param name="suit">The string to check.</param>
+        /// <returns>True is the string is a valid Suit, otherwise false.</returns>
+        public static bool IsValidSuit(string suit)
+        {
+            suit = suit.ToUpper();
+
+            if (_suitMappings.ContainsKey(suit))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Converts the Card into a string.
+        /// </summary>
+        /// <returns>A string representation of the Card.</returns>
+        public override string ToString()
+        {
+            string s = Suit.ToString().Substring(0, 1);
+            string r;
+
+            switch ((int)Rank)
+            {
+                case 8:
+                    r = "T";
+                    break;
+                case 9:
+                    r = "J";
+                    break;
+                case 10:
+                    r = "Q";
+                    break;
+                case 11:
+                    r = "K";
+                    break;
+                case 12:
+                    r = "A";
+                    break;
+                default:
+                    r = ((int)Rank + 2).ToString().Substring(0, 1);
+                    break;
+            }
+
+            return r + s;
+        }
+
+        /// <summary>
+        /// Compares the Card against a given Card to determine if their Rank and Suit are equal.
+        /// </summary>
+        /// <param name="obj">The Card object to compare.</param>
+        /// <returns>True if the given Card's Suit and Rank are the same as the caller's, otherwise false.</returns>
+        public override bool Equals(object obj)
+        {
+            return obj != null && GetType() == obj.GetType() && (this == (Card)obj);
+        }
+
+        /// <summary>
+        /// Retrieves a hash code for the Card.
+        /// </summary>
+        /// <returns>An integer hash value.</returns>
+        public override int GetHashCode()
+        {
+            return (base.GetHashCode() |
+                    Suit.GetHashCode() |
+                    Rank.GetHashCode());
+        }
+
+        /// <summary>
+        /// Compares two Cards for equality.  Two cards are equal if they are both null, or
+        /// if they both have the same Rank and Suit values.
+        /// </summary>
+        /// <param name="x">A Card.</param>
+        /// <param name="y">A Card.</param>
+        /// <returns>True if the Cards are both null, or both have the same Rank and Suit, otherwise false.</returns>
+        public static bool operator ==(Card x, Card y)
+        {
+            if ((Object)x == null && (Object)y == null)
+            {
+                return true;
+            }
+            else if ((Object)x == null || (Object)y == null)
+            {
+                return false;
+            }
+
+            return (x.Rank == y.Rank &&
+                    x.Suit == y.Suit);
+        }
+
+        /// <summary>
+        /// Determines if two Cards are not equal.
+        /// </summary>
+        /// <param name="x">A Card.</param>
+        /// <param name="y">A Card.</param>
+        /// <returns>True if the Cards, when compared with the == operator, return false.
+        /// Otherwise, returns false.</returns>
+        public static bool operator !=(Card x, Card y)
+        {
+            return !(x == y);
+        }
+    }
+}
---
+++ CS 4911 Poker Simulator/PokerSimLib4911/PokerSimLib4911/HandGenerator.cs
@@ -0,0 +1,459 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PokerSimLib4911
+{
+    public class HandGenerator
+    {
+        public HandGenerator() { }
+
+        private const int RF = 0;
+        private const int SF = 1;
+        private const int FK = 2;
+        private const int FH = 3;
+        private const int FL = 4;
+        private const int ST = 5;
+        private const int TK = 6;
+        private const int TP = 7;
+        private const int OP = 8;
+
+        private static int[] REQ_CARDS = { 5, 5, 4, 5, 5, 5, 3, 4, 2 };
+
+        //Royal Flush//JC
+        static public Hand genRF(int numCards)
+        {
+            int suit = new Random().Next(Deck.NUMBER_OF_SUITS);
+
+            // first, create a new + shuffled deck
+            Deck.Instance.MakeShuffledDeck();
+            Hand royalFlush = new Hand();
+
+            // a royal flush contains the cards ten through ace, all of the same suit
+            for (int i = 0; i < REQ_CARDS[RF]; i++)
+                royalFlush.InsertCard(new Card((Suit)suit, Rank.TEN + i));
+
+            Deck.Instance.RemoveCards(royalFlush.ToArray());
+
+            for (int i = 0; i < numCards - REQ_CARDS[RF]; i++)
+                royalFlush.InsertCard(Deck.Instance.DealCard());
+
+            royalFlush.Shuffle();
+            return royalFlush;
+        }
+
+        //Straight Flush//JSS
+        static public Hand genSF(int numCards)
+        {
+            Random r = new Random();
+            Deck deck = Deck.Instance;
+            Hand hand = null;
+
+            deck.MakeShuffledDeck();
+
+            Rank rank = (Rank)r.Next(3, 11);
+            Suit suit = (Suit)r.Next(0, 3);
+
+            hand = new Hand();
+
+            for (int i = 0; i < REQ_CARDS[SF]; i++)
+            {
+                Rank cardRank = rank - i;
+                if (cardRank < 0) cardRank = Rank.ACE;
+
+                hand.InsertCard(new Card(suit, cardRank));
+            }
+
+            deck.RemoveCards(hand.ToArray());
+
+            for (int i = 0; i < numCards - REQ_CARDS[SF]; i++)
+                hand.InsertCard(deck.DealCard());
+
+            hand.Shuffle();
+            return hand;
+        }
+
+        //Four of a Kind//JSS
+        static public Hand genFK(int numCards)
+        {
+            //Added by EJ
+            Deck.Instance.MakeShuffledDeck();
+            Hand fourKind = new Hand();
+
+            //pick the rank of the FK
+            int rank = 0;
+            Random rand = new Random();
+            rank = rand.Next(0, 12);
+
+            for (int i = 0; i < REQ_CARDS[FK]; i++)
+                fourKind.InsertCard(new Card((Suit)i, (Rank)rank));
+
+            Deck.Instance.RemoveCards(fourKind.ToArray());
+
+            //random 3 cards
+            //can be random because no cards that are dealt will make the hand better than a 4-Kind
+
+            for (int i = 0; i < numCards - REQ_CARDS[FK]; i++)
+                fourKind.InsertCard(Deck.Instance.DealCard());
+
+            fourKind.Shuffle();
+            return fourKind; //return completed hand
+        }
+
+        //Full House//Ruslan
+        static public Hand genFH(int numCards)
+        {
+            Deck deck = Deck.Instance;
+            Hand hand = new Hand();
+            Card[] dealBack = new Card[2];
+            Random r = new Random();
+
+            deck.MakeShuffledDeck();
+
+            Card[] c = new Card[] { deck.DealCard(), deck.DealCard() };
+            while (c[0].Rank == c[1].Rank)
+            {
+                deck.ReturnCard(c[1]);
+                c[1] = deck.DealCard();
+            }
+
+            Suit[,] suits = {{ Suit.CLUBS, Suit.DIAMONDS, Suit.HEARTS, Suit.SPADES },
+                        { Suit.CLUBS, Suit.DIAMONDS, Suit.HEARTS, Suit.SPADES }};
+            int suitLast = 3;
+            Rank[] ranks = { c[0].Rank, c[1].Rank };
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int a = 0; a < 2; a++)
+                {
+                    int index = r.Next(0, suitLast);
+                    Suit suit = suits[a, index];
+                    suits[a, index] = suits[a, suitLast];
+                    suits[a, suitLast] = suit;
+
+                    c[a] = new Card((Suit)suit, ranks[a]);
+                }
+
+                suitLast--;
+
+                if (i < 2) hand.InsertCards(c);
+                else dealBack[i - 2] = c[1];
+                if (i == 2) hand.InsertCard(c[0]);
+                deck.RemoveCards(c);
+            }
+
+            deck.ReturnCard(dealBack[r.Next(0, 1)]);
+
+            for (int i = 0; i < numCards - REQ_CARDS[FH]; i++)
+                hand.InsertCard(deck.DealCard());
+
+            hand.Shuffle();
+            return hand;
+        }
+
+        //Flush//JSS
+        static public Hand genFL(int numCards)
+        {
+            //Added by EJ
+            Deck.Instance.MakeShuffledDeck();
+            Hand flushHand = new Hand();
+
+            //pick the suit of the FL
+            int suit = 0;
+            Random rand = new Random();
+            suit = rand.Next(0, 3);
+            Boolean invalid = false;
+
+            Card card;
+
+            do
+            {
+                if (flushHand != null) Deck.Instance.ReturnCards(flushHand.ToArray());
+                flushHand = new Hand();
+
+                for (int i = 0; i < REQ_CARDS[FL]; i++)
+                {
+                    //choose five cards of that suit
+                    do
+                    {
+                        card = new Card((Suit)suit, (Rank)rand.Next(0, 12));
+                    } while (flushHand.Contains(card));
+
+                    flushHand.InsertCard(card);
+                }
+
+                Deck.Instance.RemoveCards(flushHand.ToArray());
+
+                for (int i = 0; i < numCards - REQ_CARDS[FL]; i++)
+                    flushHand.InsertCard(Deck.Instance.DealCard());
+
+                invalid = flushHand.HasStraight();
+            } while (invalid);
+
+            flushHand.Shuffle();
+            return flushHand; //remove later
+        }
+
+        //Straight//Ruslan
+        static public Hand genST(int numCards)
+        {
+            Random r = new Random();
+            Deck deck = Deck.Instance;
+            Hand hand = null;
+
+            deck.MakeShuffledDeck();
+
+            Rank rank = (Rank)r.Next(3, 12);
+            Boolean invalid = false;
+
+            do
+            {
+                if (hand != null) deck.ReturnCards(hand.ToArray());
+                hand = new Hand();
+
+                for (int i = 0; i < REQ_CARDS[ST]; i++)
+                {
+                    Rank cardRank = rank - i;
+                    if (cardRank < 0) cardRank = Rank.ACE;
+
+                    hand.InsertCard(new Card((Suit)r.Next(0, 3), cardRank));
+                }
+
+                deck.RemoveCards(hand.ToArray());
+
+                for (int i = 0; i < numCards - REQ_CARDS[ST]; i++)
+                    hand.InsertCard(deck.DealCard());
+
+                invalid = hand.HasFlush();
+            } while (invalid);
+
+            hand.Shuffle();
+            return hand;
+        }
+
+        //Three of a Kind//JSS
+        static public Hand genTK(int numCards)
+        {
+            //Added by EJ
+            Deck.Instance.MakeShuffledDeck();
+            Hand threeKind = new Hand();
+            Card card;
+
+            //pick the rank of the TK
+            int rank = 0;
+            Random rand = new Random();
+            rank = rand.Next(0, 12);
+
+            //Get all four cards of that rank, and randomly choose one to remove from the hand.
+            //This takes care of finding that card later to remove it from the deck so you won't
+            //end up with a 4-Kind.
+            for (int i = 0; i < REQ_CARDS[TK] + 1; i++)
+            {
+                card = new Card((Suit)i, (Rank)rank);
+                if (i < 3) threeKind.InsertCard(card);
+                Deck.Instance.RemoveCard(card);
+            }
+
+            //Generate 4 cards, knowing that a Full House, 4-Kind of a different rank, straight, flush,
+            //Straight flush and Royal Flush will beat 3-Kind.
+
+            Hand randHand = null;
+            Boolean invalid = false;
+
+            do
+            {
+                invalid = false;
+                if (randHand != null) Deck.Instance.ReturnCards(randHand.ToArray());
+                randHand = new Hand(threeKind.ToArray());
+                Deck.Instance.RemoveCards(randHand.ToArray());
+
+                for (int i = 0; i < numCards - REQ_CARDS[TK]; i++)
+                    randHand.InsertCard(Deck.Instance.DealCard());
+
+                //checks to see how many of each rank are in the hand.
+                for (int j = 0; j < 12; j++)
+                {
+                    //If there are 2 or more of a rank, and that rank is not the 3-Kind rank, hand is invalid
+                    if ((randHand.CountOf((Rank)j) >= 2) && (j != rank))
+                    {
+                        invalid = true;
+                    }
+                }
+
+                //checks for Straight and Flush in hand
+                if (invalid == false)
+                    invalid = randHand.HasStraight() || randHand.HasFlush(3);
+
+            } while (invalid);
+
+            randHand.Shuffle();
+            return randHand;
+        }
+
+        //Two Pair//Ruslan
+        static public Hand genTP(int numCards)
+        {
+            Deck deck = Deck.Instance;
+            Hand hand = new Hand();
+            Random r = new Random();
+            Card[] dealBack = new Card[2];
+
+            deck.MakeShuffledDeck();
+
+            Card[] c = new Card[] { deck.DealCard(), deck.DealCard() };
+            while (c[0].Rank == c[1].Rank)
+            {
+                deck.ReturnCard(c[1]);
+                c[1] = deck.DealCard();
+            }
+
+            Suit[,] suits = {{ Suit.CLUBS, Suit.DIAMONDS, Suit.HEARTS, Suit.SPADES },
+                        { Suit.CLUBS, Suit.DIAMONDS, Suit.HEARTS, Suit.SPADES }};
+
+            int suitLast = 3;
+
+            Rank[] ranks = new Rank[] { c[0].Rank, c[1].Rank };
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int a = 0; a < 2; a++)
+                {
+                    int index = r.Next(0, suitLast);
+                    Suit suit = suits[a, index];
+                    suits[a, index] = suits[a, suitLast];
+                    suits[a, suitLast] = suit;
+
+                    c[a] = new Card((Suit)suit, (Rank)ranks[a]);
+                }
+
+                suitLast--;
+
+                if (i < 2) hand.InsertCards(c);
+                deck.RemoveCards(c);
+            }
+
+            Hand randHand = null;
+            Boolean invalid = false;
+
+            do
+            {
+                if (randHand != null) deck.ReturnCards(randHand.ToArray());
+                randHand = new Hand(hand.ToArray());
+                deck.RemoveCards(hand.ToArray());
+
+                for (int i = 0; i < numCards - REQ_CARDS[TP]; i++)
+                    randHand.InsertCard(deck.DealCard());
+
+                invalid = randHand.HasStraight() || randHand.HasFlush();
+
+            } while (invalid);
+
+            randHand.Shuffle();
+            return randHand;
+        }
+
+        //One Pair//JSS
+        static public Hand genOP(int numCards)
+        {
+            //pick the rank of the OP
+            //pick two of the four cards of that rank
+            //Remove the other 2 cards of that rank from deck
+            //random 5 cards NOT OF THAT RANK
+
+            //Added by EJ
+            Deck.Instance.MakeShuffledDeck();
+            Hand onePair = new Hand();
+
+            //pick the rank of the TK
+            Random rand = new Random();
+            int rank = rand.Next(0, 12);
+
+            //Get all four cards of that rank, and randomly choose twp to remove from the hand.
+            //This takes care of finding those cards later to remove them from the deck so you won't
+            //end up with a 4-Kind or 3-Kind.
+            int suit, suit2 = 0;
+            suit = rand.Next(0, 3);
+            do
+            {
+                suit2 = rand.Next(0, 3);
+            } while (suit2 == suit);
+
+
+
+            for (int i = 0; i < REQ_CARDS[OP] + 2; i++)
+                onePair.InsertCard(new Card((Suit)i, (Rank)rank));
+
+            Deck.Instance.RemoveCards(onePair.ToArray());
+
+            onePair.RemoveCard(new Card((Suit)suit, (Rank)rank));
+            onePair.RemoveCard(new Card((Suit)suit2, (Rank)rank));
+
+            //Generate 5 cards, knowing that a Full House, 4-Kind of a different rank, straight, flush,
+            //Straight flush and Royal Flush will beat 2-Kind.
+
+            Hand randHand = null;
+            Boolean invalid = false;
+
+            do
+            {
+                invalid = false;
+                if (randHand != null) Deck.Instance.ReturnCards(randHand.ToArray());
+                randHand = new Hand(onePair.ToArray());
+                Deck.Instance.RemoveCards(randHand.ToArray());
+
+                for (int i = 0; i < numCards - REQ_CARDS[OP]; i++)
+                    randHand.InsertCard(Deck.Instance.DealCard());
+
+                //checks to see how many of each rank are in the hand.
+                for (int j = 0; j < 12; j++)
+                {
+                    //If there are 2 or more of a rank, and that rank is not the One Pair rank, hand is invalid
+                    if ((randHand.CountOf((Rank)j) >= 2) && (j != rank))
+                    {
+                        invalid = true;
+                    }
+                }
+                //checks for Straight and Flush in hand
+                if (!invalid)
+                    invalid = randHand.HasStraight() || randHand.HasFlush(3);
+
+            } while (invalid);
+
+            randHand.Shuffle();
+            return randHand;
+        }
+
+        //High Card//Ruslan
+        static public Hand genHC(int numCards)
+        {
+            Hand hand;
+            Deck deck = Deck.Instance;
+            Boolean invalid = false;
+
+            do
+            {
+
+                deck.MakeShuffledDeck();
+
+                hand = new Hand();
+
+                for (int i = 0; i < numCards; i++) hand.InsertCard(deck.DealCard());
+
+                Boolean hasPair = false;
+                for (int i = 0; i < 13; i++)
+                {
+                    if (hand.CountOf((Rank)i) > 1)
+                    {
+                        hasPair = true;
+                        break;
+                    }
+                }
+
+                invalid = hand.HasFlush() || hand.HasStraight() || hasPair;
+            } while (invalid);
+
+            return hand;
+        }
+    }
+}
