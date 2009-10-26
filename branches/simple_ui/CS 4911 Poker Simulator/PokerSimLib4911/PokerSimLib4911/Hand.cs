03ed60105adc4de1b77ec7907ca3fd64a19e4175
john.w.crawford <john.w.crawford@67adeefe-9b59-11de-bf47-b33d4a03a5a8>
2009-10-26 04:34:20 +0000

    Split Card, Hand and HandGenerator out of Deck.  Added helpers to Card and finished commenting.

---
+++ branches/simple_ui/CS 4911 Poker Simulator/PokerSimLib4911/PokerSimLib4911/Hand.cs
@@ -0,0 +1,592 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PokerSimLib4911
+{
+    public class Hand
+    {
+        private List<Card> _hand;
+
+        public bool IsEmpty
+        {
+            get
+            {
+                return (_hand.Count == 0);
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _hand.Count;
+            }
+        }
+
+        public Hand() : this(null) { }
+
+        public Hand(params Card[] cards)
+        {
+            _hand = new List<Card>();
+
+            if (cards != null)
+            {
+                foreach (Card c in cards)
+                {
+                    _hand.Add(c);
+                }
+            }
+
+            _hand.DefaultIfEmpty(null);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder handString = new StringBuilder();
+
+            for (int i = 0; i < _hand.Count; ++i)
+            {
+                if (i != 0)
+                {
+                    handString.Append(", ");
+                }
+                handString.Append(_hand.ElementAt(i));
+            }
+
+            return handString.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj != null && GetType() == obj.GetType() && (this == (Hand)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (base.GetHashCode() |
+                    _hand.GetHashCode());
+        }
+
+        public static bool operator ==(Hand x, Hand y)
+        {
+            if ((Object)x == null && (Object)y == null)
+            {
+                return true;
+            }
+            else if ((Object)x == null || (Object)y == null)
+            {
+                return false;
+            }
+
+            if (x._hand.Count == y._hand.Count)
+            {
+                Hand yCopy = (Hand)y.MemberwiseClone();
+
+                foreach (Card c in x._hand)
+                {
+                    if (!yCopy.RemoveCard(c))
+                    {
+                        return false;
+                    }
+                }
+            }
+            else
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool operator !=(Hand x, Hand y)
+        {
+            return !(x == y);
+        }
+
+        public bool Contains(Card card)
+        {
+            return _hand.Contains(card);
+        }
+
+        public bool Contains(Suit suit, Rank rank)
+        {
+            return Contains(new Card(suit, rank));
+        }
+
+        public bool Contains(Rank rank)
+        {
+            for (int i = 0; i < Deck.NUMBER_OF_SUITS; i++)
+            {
+                if (this.Contains((Suit)i, rank))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void InsertCard(Card card)
+        {
+            _hand.Add(card);
+        }
+
+        public void InsertCards(params Card[] cards)
+        {
+            if (cards != null)
+            {
+                foreach (Card c in cards)
+                {
+                    this.InsertCard(c);
+                }
+            }
+        }
+
+        public bool RemoveCard(Card card)
+        {
+            return _hand.Remove(card);
+        }
+
+        public bool RemoveCards(params Card[] cards)
+        {
+            bool removeSucceeded = true;
+
+            if (cards != null)
+            {
+                foreach (Card c in cards)
+                {
+                    if (!this.RemoveCard(c))
+                    {
+                        removeSucceeded = false;
+                    }
+                }
+            }
+
+            return removeSucceeded;
+        }
+
+        public void Combine(Hand hand)
+        {
+            this.InsertCards(hand.ToArray());
+        }
+
+        public int CountOf(Card card)
+        {
+            int count = 0;
+
+            foreach (Card c in _hand)
+            {
+                if (c.Suit == card.Suit && c.Rank == card.Rank)
+                {
+                    ++count;
+                }
+            }
+
+            return count;
+        }
+
+        /// <summary>
+        /// Determines the number of cards of a given suit within the hand.
+        /// </summary>
+        /// <param name="rank">
+        /// The suit of the cards the method should count.
+        /// </param>
+        /// <returns>
+        /// The number of cards of the given suit within the hand.  If no cards of the given
+        /// suit are held in the hand, then the method returns 0.
+        /// </returns>
+        public int CountOf(Suit suit)
+        {
+            int count = 0;
+
+            foreach (Card c in _hand)
+            {
+                if (c.Suit == suit)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        /// <summary>
+        /// Determines the number of cards of a given rank within the hand.
+        /// </summary>
+        /// <param name="rank">
+        /// The rank of the cards the method should count.
+        /// </param>
+        /// <returns>
+        /// The number of cards of the given rank within the hand.  If no cards of the given
+        /// rank are held in the hand, then the method returns 0.
+        /// </returns>
+        public int CountOf(Rank rank)
+        {
+            int count = 0;
+
+            foreach (Card c in _hand)
+            {
+                if (c.Rank == rank)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        /// <summary>
+        /// Orders the hand in ascending rank.
+        /// </summary>
+        public void OrderAscending()
+        {
+            _hand = _hand.OrderBy<Card, int>((card) => (int)card.Rank).ToList<Card>();
+        }
+
+        /// <summary>
+        /// Orders the hand in descending rank.
+        /// </summary>
+        public void OrderDescending()
+        {
+            _hand = _hand.OrderByDescending<Card, int>((card) => (int)card.Rank).ToList<Card>();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>
+        /// True if the hand contains at least 5 cards of sequential rank, otherwise false.
+        /// </returns>
+        public bool HasStraight()
+        {
+            if (this.HasStraight(5, Rank.ACE))
+            {
+                return true;
+            }
+            else
+            {
+                for (int i = 0; i < (int)Rank.ACE - 4; ++i)
+                {
+                    if (this.HasStraight(5, (Rank)i))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public bool HasStraight(int minimumNumberOfCards, Rank startingWithRank)
+        {
+            Hand hand = new Hand();
+            return this.HasStraight(minimumNumberOfCards, startingWithRank, ref hand);
+        }
+
+        public bool HasStraight(int minimumNumberOfCards, Rank startingWithRank, ref Hand matchedHand)
+        {
+            // an ACE can be a low card or high card
+            // in the Rank enumeration, it has the highest value other than UNKNOWN
+            // therefore, we handle the ACE being a low card as a special case
+            // first, when we've got an ACE in our hand
+            if (startingWithRank == Rank.ACE && this.GetCardsOfRank(Rank.ACE).Count > 0 && minimumNumberOfCards > 1)
+            {
+                matchedHand.InsertCard(this.GetCardsOfRank(Rank.ACE).ToArray()[0]);
+                return this.HasStraight(minimumNumberOfCards - 1, Rank.TWO, ref matchedHand);
+            }
+            // second, when we've got no ACE, but we do have a wild card (UNKNOWN)
+            else if (startingWithRank == Rank.ACE && this.GetCardsOfRank(Rank.UNKNOWN).Count > 0 && minimumNumberOfCards > 1)
+            {
+                matchedHand.InsertCard(this.GetCardsOfRank(Rank.UNKNOWN).ToArray()[0]);
+                return this.HasStraightHelper(minimumNumberOfCards - 1, Rank.TWO, this.GetCardsOfRank(Rank.UNKNOWN).Count - 1, ref matchedHand);
+            }
+            // if we're looking for a straight, consider a straight starting with a wild card to be invalid
+            else if (startingWithRank == Rank.UNKNOWN)
+            {
+                return false;
+            }
+            // we've handled the special cases, now just apply the normal algorithm
+            else
+            {
+                return this.HasStraightHelper(minimumNumberOfCards, startingWithRank, this.GetCardsOfRank(Rank.UNKNOWN).Count, ref matchedHand);
+            }
+        }
+
+        private bool HasStraightHelper(int minimumNumberOfCards, Rank startingWithRank, int numberOfWildcards)
+        {
+            Hand hand = new Hand();
+            return this.HasStraightHelper(minimumNumberOfCards, startingWithRank, numberOfWildcards, ref hand);
+        }
+
+        private bool HasStraightHelper(int minimumNumberOfCards, Rank startingWithRank, int numberOfWildcards, ref Hand matchedHand)
+        {
+            int highestRank = (int)Rank.ACE;
+            int endRank = (int)startingWithRank + minimumNumberOfCards - 1;
+
+            // cannot form a straight with less than one card
+            if (minimumNumberOfCards < 1)
+            {
+                return false;
+            }
+
+            // cannot form an X card straight from less than X number of cards
+            if (highestRank < endRank)
+            {
+                return false;
+            }
+
+            if (this.GetCardsOfRank(startingWithRank).Count > 0)
+            {
+                matchedHand.InsertCard(this.GetCardsOfRank(startingWithRank).PeekHighCard());
+                if (minimumNumberOfCards > 1)
+                {
+                    return this.HasStraightHelper(minimumNumberOfCards - 1, startingWithRank + 1, numberOfWildcards, ref matchedHand);
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else if (numberOfWildcards > 0)
+            {
+                matchedHand.InsertCard(new Card(Suit.UNKNOWN, Rank.UNKNOWN));
+                if (minimumNumberOfCards > 1)
+                {
+                    return this.HasStraightHelper(minimumNumberOfCards - 1, startingWithRank + 1, numberOfWildcards - 1, ref matchedHand);
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool HasFlush()
+        {
+            return HasFlush(5);
+        }
+
+        public bool HasFlush(int minimumNumberOfCards)
+        {
+            for (int i = 0; i < 4; ++i)
+            {
+                if (HasFlush(minimumNumberOfCards, (Suit)i))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool HasFlush(int minimumNumberOfCards, Suit suit)
+        {
+            Hand hand = new Hand();
+            return this.HasFlush(minimumNumberOfCards, suit, ref hand);
+        }
+
+        public bool HasFlush(int minimumNumberOfCards, Suit suit, ref Hand matchedHand)
+        {
+            int count = this.GetSuitCards(suit).Count;
+
+            // have to have at least a single card for a flush
+            if (minimumNumberOfCards < 1)
+            {
+                return false;
+            }
+
+            // if the suit isn't unknown, then we can add the wild cards to the suit's total number of cards
+            if (suit != Suit.UNKNOWN)
+            {
+                count = this.GetSuitCards(suit).Count + this.GetSuitCards(Suit.UNKNOWN).Count;
+            }
+            // make sure the the total number of cards of this suit is adequate
+            if ((this.GetSuitCards(suit).Count + this.GetSuitCards(Suit.UNKNOWN).Count) >= minimumNumberOfCards)
+            {
+                matchedHand = GetSuitCards(suit);
+                if (suit != Suit.UNKNOWN)
+                {
+                    matchedHand.Combine(GetSuitCards(Suit.UNKNOWN));
+                }
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool HasRoyalFlush(int minimumNumberOfCards, Suit suit, ref Hand matchedHand)
+        {
+            Rank highestRank = Rank.ACE;
+            Rank startingWithRank = highestRank - minimumNumberOfCards + 1;
+            Hand flush = new Hand();
+            Hand royalFlush = new Hand();
+
+            if (this.HasFlush(minimumNumberOfCards, suit, ref flush))
+            {
+                if (flush.HasStraight(minimumNumberOfCards, startingWithRank, ref royalFlush))
+                {
+                    matchedHand = royalFlush;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool HasStraightFlush(int minimumNumberOfCards, Rank startingWithRank, Suit suit, ref Hand matchedHand)
+        {
+            Hand flush = new Hand();
+            Hand straightFlush = new Hand();
+
+            if (this.HasFlush(minimumNumberOfCards, suit, ref flush))
+            {
+                if (flush.HasStraight(minimumNumberOfCards, startingWithRank, ref straightFlush))
+                {
+                    matchedHand = straightFlush;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool HasFourOfAKind(Rank ofRank, ref Hand matchedHand)
+        {
+            return HasXOfAKind(ofRank, 4, ref matchedHand);
+        }
+
+        public bool HasThreeOfAKind(Rank ofRank, ref Hand matchedHand)
+        {
+            return HasXOfAKind(ofRank, 3, ref matchedHand);
+        }
+
+        public bool HasTwoOfAKind(Rank ofRank, ref Hand matchedHand)
+        {
+            return HasPairOf(ofRank, ref matchedHand);
+        }
+
+        public bool HasPairOf(Rank ofRank, ref Hand matchedHand)
+        {
+            return HasXOfAKind(ofRank, 2, ref matchedHand);
+        }
+
+        public bool HasXOfAKind(Rank ofRank, int x, ref Hand matchedHand)
+        {
+            if (GetCardsOfRank(ofRank).Count == x)
+            {
+                matchedHand.InsertCards(GetCardsOfRank(ofRank).ToArray());
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool HasTwoPair(Rank firstRank, Rank secondRank, ref Hand matchedHand)
+        {
+            return (HasPairOf(firstRank, ref matchedHand) && HasPairOf(secondRank, ref matchedHand));
+        }
+
+        public bool HasFullHouse(Rank tripleRank, Rank pairRank, ref Hand matchedHand)
+        {
+            return (HasThreeOfAKind(tripleRank, ref matchedHand) && HasPairOf(pairRank, ref matchedHand));
+        }
+
+        public Card PeekHighCard()
+        {
+            Hand ordered = new Hand(_hand.ToArray());
+            ordered.OrderAscending();
+
+            if (ordered.Count > 0)
+            {
+                return ordered.ToArray()[ordered.Count - 1];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public Card[] ToArray()
+        {
+            return _hand.ToArray();
+        }
+
+        public Hand GetSuitCards(Suit suit)
+        {
+            Hand hand = new Hand();
+
+            foreach (Card c in _hand)
+            {
+                if (c.Suit == suit)
+                {
+                    hand.InsertCard(c);
+                }
+            }
+
+            return hand;
+        }
+
+        public void Shuffle()
+        {
+            List<Card> tempCards = new List<Card>();
+            Random rand = new Random();
+
+            while (_hand.Count > 0)
+            {
+                // select a card from the cards left in the hand
+                int randomIndex = rand.Next(_hand.Count);
+                // add the selected card to the temp hand
+                tempCards.Add(_hand[randomIndex]);
+                // remove the selected card from the current hand
+                _hand.RemoveAt(randomIndex);
+            }
+
+            // set the current hand to the shuffled hand
+            _hand = tempCards;
+        }
+
+        public Hand GetClubs()
+        {
+            return GetSuitCards(Suit.CLUBS);
+        }
+
+        public Hand GetDiamonds()
+        {
+            return GetSuitCards(Suit.DIAMONDS);
+        }
+
+        public Hand GetHearts()
+        {
+            return GetSuitCards(Suit.HEARTS);
+        }
+
+        public Hand GetSpades()
+        {
+            return GetSuitCards(Suit.SPADES);
+        }
+
+        public Hand GetCardsOfRank(Rank rank)
+        {
+            Hand hand = new Hand();
+
+            foreach (Card c in _hand)
+            {
+                if (c.Rank == rank)
+                {
+                    hand.InsertCard(c);
+                }
+            }
+
+            return hand;
+        }
+    }
+
+}
