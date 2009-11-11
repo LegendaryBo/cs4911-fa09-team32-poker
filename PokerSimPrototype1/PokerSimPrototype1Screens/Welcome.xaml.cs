4925920cac820ce9b6f662119c0b587a0beb9bd0
john.w.crawford <john.w.crawford@67adeefe-9b59-11de-bf47-b33d4a03a5a8>
2009-11-11 03:18:26 +0000

    UI Prototype.

---
+++ PokerSimPrototype1/PokerSimPrototype1Screens/Welcome.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PokerSimPrototype1Screens
+{
+	/// <summary>
+	/// Interaction logic for Screen_1.xaml
+	/// </summary>
+	public partial class Screen_1 : UserControl
+	{
+		public Screen_1()
+		{
+			this.InitializeComponent();
+		}
+
+		private void txtbx_subjectID_GotKeyboardFocus(object sender, System.Windows.Input.KeyboardFocusChangedEventArgs e)
+		{
+			if(txtbx_subjectID.Text == "Subject ID")
+			{
+				txtbx_subjectID.Text = "";
+			}
+		}
+
+		private void txtbx_subjectID_LostKeyboardFocus(object sender, System.Windows.Input.KeyboardFocusChangedEventArgs e)
+		{
+			if(txtbx_subjectID.Text == "")
+			{
+				txtbx_subjectID.Text = "Subject ID";
+			}
+		}
+
+		private void txtbx_sessionID_GotKeyboardFocus(object sender, System.Windows.Input.KeyboardFocusChangedEventArgs e)
+		{
+			if(txtbx_sessionID.Text == "Session ID")
+			{
+				txtbx_sessionID.Text = "";
+			}
+		}
+
+		private void txtbx_sessionID_LostKeyboardFocus(object sender, System.Windows.Input.KeyboardFocusChangedEventArgs e)
+		{
+			if(txtbx_sessionID.Text == "")
+			{
+				txtbx_sessionID.Text = "Session ID";
+			}
+		}
+	}
+}
