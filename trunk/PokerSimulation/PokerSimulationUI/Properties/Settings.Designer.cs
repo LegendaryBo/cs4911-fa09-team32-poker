fb8e4da74cce13b9a704d8405e2800b5c71ccd98
john.w.crawford <john.w.crawford@67adeefe-9b59-11de-bf47-b33d4a03a5a8>
2009-12-04 06:33:40 +0000

    Implemented feedback.

--- trunk/PokerSimulation/PokerSimulationUI/Properties/Settings.Designer.cs
+++ trunk/PokerSimulation/PokerSimulationUI/Properties/Settings.Designer.cs
@@ -37,7 +37,7 @@ namespace PokerSimulationUI.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool ShowFeedback {
             get {
                 return ((bool)(this["ShowFeedback"]));
